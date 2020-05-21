26fd35080a7c8913d60a584ca406143f1432242c
jonapoul <jonathanpoulton1@gmail.com>
2020-05-21 16:36:12 +0100

    Added the option to check for the most recent Github release

---
+++ app/src/main/java/com/jon/cotbeacon/updating/GithubApi.java
@@ -0,0 +1,11 @@
+package com.jon.cotbeacon.updating;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GithubApi {
+    @GET("/repos/jonapoul/cotbeacon/releases")
+    Call<List<JsonRelease>> getAllReleases();
+}
